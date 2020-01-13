9f6490c47fd2c15186ed0cb4b7445ef5b4f39520
Silviu Bogan <silviubogan@gmail.com>
2020-01-13 15:20:25 +0200

    Initial commit

---
+++ Assets/SphereResizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereResizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // what should I put here?
+        GameObject.Find("Sphere").transform.localScale = new Vector3(1f, 1f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
